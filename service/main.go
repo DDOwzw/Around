666c576fd928b36be727d9bfa25617684004a032
DDOwzw <zwwang0816@gmail.com>
2018-12-19 00:31:12 -0600

    GCS credential sucksgit status!

--- service/main.go
+++ service/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"reflect"
 	"strconv"
+
+	"cloud.google.com/go/storage"
+	"github.com/google/uuid"
+	"github.com/olivere/elastic"
 )
 
 const (
-	DISTANCE = "200km"
+	POST_INDEX = "post"
+	POST_TYPE  = "post"
+	DISTANCE   = "200km"
+
+	// Needs to update this URL if deploied to cloud.
+	ES_URL      = "http://35.237.107.132:9200"
+	BUCKET_NAME = "asuna-post-image-233"
 )
 
 type Location struct {
@@ -22,26 +35,191 @@ type Post struct {
 	User     string   `json:"user"`
 	Message  string   `json:"message"`
 	Location Location `json:"location"`
+	Url      string   `json:"url"`
 }
 
 func main() {
 	fmt.Println("started-service")
+	createIndexIfNotExist()
+
 	http.HandleFunc("/post", handlerPost)
 	http.HandleFunc("/search", handlerSearch)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func handlerPost(w http.ResponseWriter, r *http.Request) {
-	// Parse from body of request to get a json object.
-	fmt.Println("Received one post request")
-	decoder := json.NewDecoder(r.Body)
-	var p Post
-	if err := decoder.Decode(&p); err != nil {
+func createIndexIfNotExist() {
+	//                                                       check backend is normal
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+	}
+	exists, err := client.IndexExists(POST_INDEX).Do(context.Background())
+	if err != nil {
 		panic(err)
 	}
 
-	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+	if !exists {
+		mapping := `{
+			"mappings": {
+				"post": {
+					"properties": {
+						"location": {
+							"type": "geo_point"
+						}
+					}
+				}
+			}
+		}`
+		_, err = client.CreateIndex(POST_INDEX).Body(mapping).Do(context.Background())
+		if err != nil {
+			panic(err)
+		}
+	}
+}
 
+// Save a post to ElasticSearch
+func saveToES(post *Post, id string) error {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		return err
+	}
+	_, err = client.Index().
+		Index(POST_INDEX).
+		Type(POST_TYPE).
+		Id(id).
+		BodyJson(post).
+		Refresh("wait_for").
+		Do(context.Background())
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Post is saved to index: %s\n", post.Message)
+	return nil
+}
+
+func readFromES(lat, lon float64, ran string) ([]Post, error) {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		return nil, err
+	}
+
+	query := elastic.NewGeoDistanceQuery("location")
+	query = query.Distance(ran).Lat(lat).Lon(lon)
+
+	searchResult, err := client.Search().
+		Index(POST_INDEX).
+		Query(query).
+		Pretty(true).
+		Do(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	// Each is a convenience function that iterates over hits in a search result.
+	// It makes sure you don't need to check for nil values in the response.
+	// However, it ignores errors in serialization. If you want full control
+	// over iterating the hits, see below.
+	var ptyp Post
+	var posts []Post
+	for _, item := range searchResult.Each(reflect.TypeOf(ptyp)) {
+		if p, ok := item.(Post); ok {
+			posts = append(posts, p)
+		}
+	}
+
+	return posts, nil
+
+}
+
+func saveToGCS(r io.Reader, bucketName, objectName string) (*storage.ObjectAttrs, error) {
+	ctx := context.Background()
+
+	// Sets your Google Cloud Platform project ID.
+	// projectID := "Guess-My-ID"
+
+	// Creates a client.
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	bucket := client.Bucket(bucketName)
+	_, err = bucket.Attrs(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	object := bucket.Object(objectName)
+	wc := object.NewWriter(ctx)
+	if _, err = io.Copy(wc, r); err != nil {
+		return nil, err
+	}
+	if err := wc.Close(); err != nil {
+		return nil, err
+	}
+
+	// if err = object.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+	// 	return nil, err
+	// }
+
+	attrs, err := object.Attrs(ctx)
+	if err != nil {
+		return nil, nil
+	}
+	// [END upload_file]
+	fmt.Printf("Image is saved to GCS: %s\n", attrs.MediaLink)
+	return attrs, nil
+
+}
+
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+	// Parse from body of request to get a json object.
+	fmt.Println("Received one post request")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+	// decoder := json.NewDecoder(r.Body)
+	// var p Post
+	// if err := decoder.Decode(&p); err != nil {
+	// 	http.Error(w, "Cannot decode post data from client", http.StatusBadRequest)
+	// 	fmt.Printf("Cannot decode post data from client: %v\n", err)
+	// 	return
+	// }
+
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+
+	p := &Post{
+		User:    r.FormValue("user"),
+		Message: r.FormValue("message"),
+		Location: Location{
+			Lat: lat,
+			Lon: lon,
+		},
+	}
+	id := uuid.New().String()
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Image is not available", http.StatusBadRequest)
+		fmt.Printf("Image is not available %v.\n", err)
+		return
+	}
+	attrs, err := saveToGCS(file, BUCKET_NAME, id)
+	if err != nil {
+		http.Error(w, "Failed to save post to GCS", http.StatusInternalServerError)
+		fmt.Printf("Failed to save post to GCS %v.\n", err)
+		return
+	}
+	p.Url = attrs.MediaLink
+
+	err = saveToES(p, id)
+	if err != nil {
+		http.Error(w, "Failed to save post to ElasticSearch", http.StatusInternalServerError)
+		fmt.Printf("Failed to save post to ElasticSearch %v.\n", err)
+		return
+	}
+	fmt.Printf("Saved one post to ElasticSearch: %s", p.Message)
 }
 
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
@@ -56,21 +234,19 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("range is ", ran)
 
-	// Return a fake post
-	p := &Post{
-		User:    "1111",
-		Message: "一生必去的100个地方",
-		Location: Location{
-			Lat: lat,
-			Lon: lon,
-		},
-	}
-
-	js, err := json.Marshal(p)
+	posts, err := readFromES(lat, lon, ran)
 	if err != nil {
-		panic(err)
+		http.Error(w, "Failed to read post from ElasticSearch", http.StatusInternalServerError)
+		fmt.Printf("Failed to read post from ElasticSearch %v.\n", err)
+		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	js, err := json.Marshal(posts)
+	if err != nil {
+		http.Error(w, "Failed to parse posts into JSON format", http.StatusInternalServerError)
+		fmt.Printf("Failed to parse posts into JSON format %v.\n", err)
+		return
+	}
+
 	w.Write(js)
 }
