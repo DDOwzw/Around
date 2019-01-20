55b5168cf481cf723357e12027c56cc049119633
DDOwzw <zwwang0816@gmail.com>
2019-01-19 20:35:47 -0600

    Added face recognize with google ML engine

--- service/main.go
+++ service/main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"path/filepath"
 	"reflect"
 	"strconv"
 
@@ -26,7 +27,7 @@ const (
 	DISTANCE   = "200km"
 
 	// Needs to update this URL if deploied to cloud.
-	ES_URL      = "http://35.243.254.68:9200"
+	ES_URL      = "http://34.73.185.248:9200"
 	BUCKET_NAME = "around-bucket"
 
 	CRED            = "../My Project-23d7224dd687.json"
@@ -35,6 +36,9 @@ const (
 	BIGTABLE_PROJECT_ID = "my-project-1523483736723"
 	BT_INSTANCE         = "around-post"
 	mySigningKey        = "secret"
+	ENABLE_AUTH         = true
+	// REDIS_URL           = "redis-18610.c1.us-central1-2.gce.cloud.redislabs.com:18610"
+	API_PREFIX = "/api/v1"
 )
 
 type Location struct {
@@ -48,7 +52,23 @@ type Post struct {
 	Message  string   `json:"message"`
 	Location Location `json:"location"`
 	Url      string   `json:"url"`
+	Type     string   `json:"type"`
+	Face     float64  `json:"face"`
 }
+
+var (
+	mediaTypes = map[string]string{
+		".jpeg": "image",
+		".jpg":  "image",
+		".gif":  "image",
+		".png":  "image",
+		".mov":  "video",
+		".mp4":  "video",
+		".avi":  "video",
+		".flv":  "video",
+		".wmv":  "video",
+	}
+)
 
 func main() {
 	fmt.Println("started-service")
@@ -62,13 +82,16 @@ func main() {
 	})
 
 	r := mux.NewRouter()
+	r.Handle(API_PREFIX+"/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost))).Methods("POST", "OPTIONS")
+	r.Handle(API_PREFIX+"/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch))).Methods("GET", "OPTIONS")
+	r.Handle(API_PREFIX+"/cluster", jwtMiddleware.Handler(http.HandlerFunc(handlerCluster))).Methods("GET", "OPTIONS")
 
-	r.Handle("/signup", http.HandlerFunc(handlerSignup)).Methods("POST")
-	r.Handle("/login", http.HandlerFunc(handlerLogin)).Methods("POST")
-	r.Handle("/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost))).Methods("POST")
-	r.Handle("/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch))).Methods("GET")
+	r.Handle(API_PREFIX+"/login", http.HandlerFunc(handlerLogin)).Methods("POST", "OPTIONS")
+	r.Handle(API_PREFIX+"/signup", http.HandlerFunc(handlerSignup)).Methods("POST", "OPTIONS")
 
-	http.Handle("/", r)
+	// Backend endpoints.
+	http.Handle(API_PREFIX+"/", r)
+	fmt.Println("Start service")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -278,8 +301,8 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Failed to save post to GCS %v.\n", err)
 		return
 	}
-	p.Url = attrs.MediaLink
 
+	p.Url = attrs.MediaLink
 	err = saveToES(p, id)
 	if err != nil {
 		http.Error(w, "Failed to save post to ElasticSearch", http.StatusInternalServerError)
@@ -292,6 +315,27 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 		saveToBigTable(p, id)
 	}
 	fmt.Printf("Saved one post to ElasticSearch: %s", p.Message)
+
+	im, header, _ := r.FormFile("image")
+	defer im.Close()
+	suffix := filepath.Ext(header.Filename)
+
+	// Client needs to know the media type so as to render it.
+	if t, ok := mediaTypes[suffix]; ok {
+		p.Type = t
+	} else {
+		p.Type = "unknown"
+	}
+	// ML Engine only supports jpeg.
+	if suffix == ".jpeg" {
+		if score, err := annotate(im); err != nil {
+			http.Error(w, "Failed to annotate the image", http.StatusInternalServerError)
+			fmt.Printf("Failed to annotate the image %v\n", err)
+			return
+		} else {
+			p.Face = score
+		}
+	}
 }
 
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
@@ -317,6 +361,76 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, "Failed to parse posts into JSON format", http.StatusInternalServerError)
 		fmt.Printf("Failed to parse posts into JSON format %v.\n", err)
+		return
+	}
+
+	w.Write(js)
+}
+
+func handlerCluster(w http.ResponseWriter, r *http.Request) {
+	// Parse from body of request to get a json object.
+	fmt.Println("Received one post request")
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+	if r.Method == "OPTIONS" {
+		return
+	}
+
+	if r.Method != "GET" {
+		return
+	}
+
+	term := r.URL.Query().Get("term")
+
+	// Create a client
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		http.Error(w, "ES is not setup", http.StatusInternalServerError)
+		fmt.Printf("ES is not setup %v\n", err)
+		return
+	}
+
+	// Range query.
+	// For details, https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-range-query.html
+	q := elastic.NewRangeQuery(term).Gte(0.9)
+
+	searchResult, err := client.Search().
+		Index(POST_INDEX).
+		Query(q).
+		Pretty(true).
+		Do(context.Background())
+
+	if err != nil {
+		// Handle error
+		m := fmt.Sprintf("Failed to query ES %v", err)
+		fmt.Println(m)
+		http.Error(w, m, http.StatusInternalServerError)
+	}
+
+	// searchResult is of type SearchResult and returns hits, suggestions,
+	// and all kinds of other information from Elasticsearch.
+	fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
+	// TotalHits is another convenience function that works even when something goes wrong.
+	fmt.Printf("Found a total of %d post\n", searchResult.TotalHits())
+
+	// Each is a convenience function that iterates over hits in a search result.
+	// It makes sure you don't need to check for nil values in the response.
+	// However, it ignores errors in serialization.
+	var typ Post
+	var ps []Post
+	for _, item := range searchResult.Each(reflect.TypeOf(typ)) {
+		p := item.(Post)
+		ps = append(ps, p)
+
+	}
+	js, err := json.Marshal(ps)
+	if err != nil {
+		m := fmt.Sprintf("Failed to parse post object %v", err)
+		fmt.Println(m)
+		http.Error(w, m, http.StatusInternalServerError)
 		return
 	}
 
